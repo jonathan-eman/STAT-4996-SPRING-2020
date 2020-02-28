f8cec17e92c9b2cad8f6c23c45c25ef6e07c4a23
Jonathan Eman <jonathaneman57@gmail.com>
2020-02-28 14:48:40 -0500

    Update filter for subset

--- Jonathan/EDA2.R
+++ Jonathan/EDA2.R
@@ -11,6 +11,12 @@
 setwd("~/R/STAT 4996")
 
 library(tidyverse)
+library(ggcorrplot)
+library(ROCR)
+library(MASS)
+library(klaR)
+library(ICS)
+library(ISLR)
 
 # Read-in data
 data_2019 <- read.csv("2019 PFF All Plays.csv")
@@ -38,27 +44,69 @@ data_2019 %>%
       ),
       OFF_TEAM = ifelse(HOME_SCORE == pff_OFFSCORE, "Home", "Away"),
       DEF_TEAM = ifelse(HOME_SCORE == pff_DEFSCORE, "Home", "Away"),
-      TIME_REMAINING = round(as.numeric(MINS) + as.numeric(SECS)/60, 2)
+      TIME_REMAINING = round(as.numeric(MINS) + as.numeric(SECS)/60, 2),
+      pff_DOWN = as.factor(pff_DOWN),
+      pff_QUARTER = as.factor(pff_QUARTER),
+      YDS_TO_END_ZONE = ifelse(pff_FIELDPOSITION < 0,
+                              abs(pff_FIELDPOSITION) + 50,
+                              pff_FIELDPOSITION)
    ) %>%
    filter(pff_SCOREDIFFERENTIAL < 0,
-          pff_QUARTER == 4,
-          TIME_REMAINING <= 5) -> subset_2019
+          pff_QUARTER = 4,
+          pff_DOWN != 0,
+          TIME_REMAINING <= 10) -> subset_2019
 
 subset_2019 %>%
    group_by(pff_GAMEID) %>%
    slice(n()) %>%
    mutate(WINNER = ifelse(HOME_SCORE > AWAY_SCORE, "Home", "Away")) %>%
-   select(pff_GAMEID, HOME_SCORE, AWAY_SCORE, WINNER) -> winners_2019
+   dplyr::select(pff_GAMEID, HOME_SCORE, AWAY_SCORE, WINNER) -> winners_2019
 
 subset_2019 %>%
    select(pff_GAMEID, pff_QUARTER, TIME_REMAINING, pff_SCOREDIFFERENTIAL, 
           pff_DISTANCE, pff_FIELDPOSITION, OFF_TEAM, DEF_TEAM, pff_OFFSCORE,
           pff_DEFSCORE) %>% head()
 
-### Win probability model
 subset_2019 %>%
    left_join(winners_2019, by = "pff_GAMEID") %>% 
-   mutate(TEAM_WINS = ifelse(OFF_TEAM == WINNER, 1, 0))
+   mutate(TEAM_WINS = as.factor(ifelse(
+      OFF_TEAM == WINNER, 1, 0))) -> join_2019
+
+set.seed(12211999)
+
+# split data
+sample.data <- sample.int(nrow(join_2019), floor(.50*nrow(join_2019)),
+                          replace = F)
+train <- join_2019[sample.data, ]
+test <- join_2019[-sample.data, ] 
+
+
+### Win probability model
+
+glm(data = train, formula = TEAM_WINS ~ pff_QUARTER + pff_DOWN +
+    TIME_REMAINING + pff_DISTANCE + YDS_TO_END_ZONE + pff_SCOREDIFFERENTIAL,
+    family = "binomial") -> win_prob_model
+
+summary(win_prob_model)
+
+# get predictions and create ROC curve
+preds_logistic <- predict(win_prob_model, newdata=test, type = "response")
+
+rates_logistic <- prediction(preds_logistic, test$TEAM_WINS)
+
+roc_logistic <- performance(rates_logistic, measure = "tpr", x.measure = "fpr")
+
+plot(roc_logistic, main="ROC Curve")
+lines(x = c(0,1), y = c(0,1), col="red")
+
+# confusion matrix and overall error rate
+confusion.mat.logistic <- table(test$TEAM_WINS, preds_logistic > 0.5)
+
+overall.error.logistic <- (confusion.mat.logistic[1,2] + 
+                              confusion.mat.logistic[2,1]) / 
+   sum(confusion.mat.logistic) 
+
+# Overall error rate with logistic regression is 13.7%
 
 
 
